67ca217fa7c79785643452408fe2f2871b0647ee
RahulNalawade2000 <nalawade.rahul20@gmail.com>
2022-05-20 18:47:10 +0530

    home add

---
+++ BookReview/JS/script.js
@@ -0,0 +1,12 @@
+﻿
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
