3fbb7ae2b8d4a4d99cf78d0d77ae301fef9ea1be
vivekbmraut <vivek.bm.raut@gmail.com>
2022-05-26 20:17:25 +0530

    navbar-profile-search

---
+++ BookReview/JS/adminHome.js
@@ -0,0 +1,11 @@
+﻿let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+let viewProfile = document.querySelector(".profile");
+
+document.querySelector('#details').onclick = () => {
+    viewProfile.classList.toggle('active');
+}
