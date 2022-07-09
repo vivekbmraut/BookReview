1fb5d08db1f620432efe8ccd8161e22777f6b511
vivekbmraut <vivek.bm.raut@gmail.com>
2022-07-09 12:03:52 +0530

    addBook

--- BookReview/user/bookDetails.aspx.cs
+++ BookReview/user/bookDetails.aspx.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Book = BookReview.Models.Book;
+using BookReview.Services;
 
 namespace BookReview.html
 {
@@ -11,7 +14,23 @@ namespace BookReview.html
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
+            if(Request["bid"]!=null)
+            {
+                Book? book = BookServices.Get(Convert.ToInt32(Request["bid"]));
+                if (book != null)
+                {
+                    bookCover.Attributes.Add("src", $"../bookCovers/{book.book_cover}");
+                    title.InnerText = book.title;
+                    authors.InnerText = book.authors;
+                    description.InnerText = book.description;
+                    category.InnerText = book.category;
+                    year.InnerText = book.year;
+                    edition.InnerText = book.edition.ToString();
+                    language.InnerText = book.language;
+                    publisher.InnerText = book.publisher;
+                }
+            }
         }
     }
 }
